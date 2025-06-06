af6aac55a8cdefac6ffd291acd626159b5188e5f
Asritha152 <161278212+Asritha152@users.noreply.github.com>
2025-06-06 20:51:38 +0530

    bug fix

--- backend/config/mongodb-connection.js
+++ backend/config/mongodb-connection.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 require('dotenv').config()
-mongoose.connect(`${process.env.MONGO_URI}`).then(()=>{
+mongoose.connect(`${process.env.MONGO_URI}devTinder`).then(()=>{
     
     
     console.log("mongodb connected")
